1d821f5f509ffbfd6e08bde2649547eaa8ada141
Roger Mushagalusa Biringanine <rogetheophile2000@gmail.com>
2024-07-24 16:26:09 +0200

    new poo

---
+++ gestion_Personnel/Personne.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Personne
+{
+private:
+    /* data */
+    int id;
+    int age;
+
+public:
+    Personne(/* args */);
+    ~Personne();
+};
+
+Personne::Personne(/* args */)
+{
+}
+
+Personne::~Personne()
+{
+}
